0d2d6dcf5504e0031ad0e160b4654fa1bc8b1432
Christopher House <chris.house.00@gmail.com>
2013-11-05 12:34:10 -0600

    Added some comments, reorganized stuff

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-CSharpTypFromString
-===================
+Parsing Types from Strings in C#
+======================
+
+This is a small project that demonstrates some of the foibles of parsing generic types in C# from strings
--- ThirteenDaysAWeek.TypeLoadFromString.Tests/UnitTest1.cs
+++ ThirteenDaysAWeek.TypeLoadFromString.Tests/UnitTest1.cs
@@ -5,8 +5,27 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ThirteenDaysAWeek.TypeLoadFromString.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class TypeLoadTests
     {
+        [TestMethod]
+        public void GetType_Should_Return_Null_For_FullName()
+        {
+            // Arrange
+            Type theType = typeof(Domain<CustomerModel>);
+            string fullName = theType.FullName;
+
+            // Act
+            // FullName will have the fully qualified name for any generic type parameters but *NOT* for the generic type itself.
+            // This results in loadedType being null when we call Type.GetType because the type we're trying to load is in another
+            // assembly.  By default, Type.GetType searches the current assembly and mscorlib only.  Since the generic type isn't
+            // fully qualified, Type.GetType will return null.
+            Type loadedType = Type.GetType(fullName);
+
+            // Assert
+            loadedType.Should()
+                .BeNull();
+        }
+
         [TestMethod]
         public void GetType_Should_Return_Type_Instance_For_AssemblyQualifiedName()
         {
@@ -15,26 +34,14 @@ namespace ThirteenDaysAWeek.TypeLoadFromString.Tests
             string assemblyQualifiedName = theType.AssemblyQualifiedName;
 
             // Act
+            // AssemblyQualifiedName will have the fully qualified type name for both the generic type as well as any type parameters.
+            // Since the generic type and it's parameter type are located in a separate assembly but are loaded using fully qualified
+            // names, Type.GetType will return an instance of the specified type.
             Type loadedType = Type.GetType(assemblyQualifiedName);
 
             // Assert
             loadedType.Should()
                 .NotBeNull();
-        }
-
-        [TestMethod]
-        public void GetType_Should_Return_Null_For_FullName()
-        {
-            // Arrange
-            Type theType = typeof (Domain<CustomerModel>);
-            string fullName = theType.FullName;
-
-            // Act
-            Type loadedType = Type.GetType(fullName);
-
-            // Assert
-            loadedType.Should()
-                .BeNull();
         }
     }
 }
